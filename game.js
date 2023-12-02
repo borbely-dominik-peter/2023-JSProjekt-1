fcc77acb0a804f996d356b27c2044c5a47b2e1e2
borbely-dominik-peter <98750586+borbely-dominik-peter@users.noreply.github.com>
2023-12-02 08:58:39 +0100

    game.js V0.1
    A játék logikai alapjai készen vannak

--- game.js
+++ game.js
@@ -9,7 +9,14 @@ class char {
     
 }
 
-let PNAME = "test";
+let S1E = new char(200, 50, 20, 20, "testENEMY1");
+let S1P = new char(500, 80, 10, 30, "test");
+
+const EMAXHP = S1E.HP;
+const EMAXDEF = S1E.DEF;
+const PMAXDEF = S1P.DEF;
+const Choices = [1,2,3];
+/*let PNAME = "test";
 let PHP = 500;
 let PATK = 80;
 let PDEF = 10;
@@ -19,9 +26,11 @@ let EHP = 200;
 let EATK = 50;
 let EDEF = 20;
 let EMANA = 20;
+*/
 let ETurn = 0;
 let PTurn = 0;
 let PMAction;
+
 
 function menu(PNAME, PHP, PATK, PDEF, PMANA,ENAME, EHP, EATK, EDEF, EMANA) {
     console.log(`You are ${PNAME}`);
@@ -39,9 +48,9 @@ function menu(PNAME, PHP, PATK, PDEF, PMANA,ENAME, EHP, EATK, EDEF, EMANA) {
 
 function help(){
     breaker = false;
-    while (breaker === true){
+    while (breaker === false){
         let PMAction = prompt("Choose the item you want help on [1-Slice,2-Shield,3-Restore]")
-        while (PMAction != '1' || PMAction != '2' || PMAction != '3'){
+        while (!Choices.includes(Number(PMAction))){
             alert("input check failed, please try again")
             let PMAction = prompt("Choose the item you want help on [1-Slice,2-Shield,3-Restore]");
         }
@@ -72,29 +81,32 @@ function help(){
     }
 }
 
-const EMAXHP = EHP;
-const EMAXDEF = EDEF;
-const PMAXDEF = PDEF;
-const Choices = [1,2,3];
+
 function AI(EHP, EATK, EDEF, EMANA, EMAXHP) {
     EChoice = -1 // 1 - atk, 2 - slice, 3 - shield, 4 - restore
     let randnum = Math.floor(Math.random() * 100) + 1;
+    alert(`${typeof(randnum)} ${randnum}`)
     if (EMANA < 7 && randnum < 50){
+        alert("AI CHOSE 1");
         EChoice = 1;
         return EChoice
     }
-    if(EMANA >= 15 && randnum >= 70){
+    else if(EMANA >= 15 && randnum >= 70){
+        alert("AI CHOSE 2");
         EChoice = 2;
         return EChoice;
     }
     else if(EMANA >= 10 && randnum >= 50){
+        alert("AI CHOSE 3");
         EChoice = 3;
         return EChoice;
     }
     else if(EMANA >= 7 && randnum >= 50 && EHP <= EMAXHP / 2){
+        alert("AI CHOSE 4");
         EChoice = 4;
         return EChoice;
     }
+    alert("AI DEFAULTED");
     return 1;
 }
 
@@ -104,22 +116,21 @@ function battle(PNAME, PHP, PATK, PDEF, PMANA, ENAME, EHP, EATK, EDEF, EMANA){
     while (PHP > 0 || EHP > 0){
         // menu
         menu(PNAME, PHP, PATK, PDEF, PMANA,ENAME, EHP, EATK, EDEF, EMANA);
-        PMAction = prompt("Choose your action [1-atk, 2-Magic move,3-help]")
-        /*while (!Choices.includes(PMAction)){
-            console.log(typeof(PMAction));
+        PMAction = prompt("Choose your action [1-atk, 2-Magic move,3-help]");
+        while (!Choices.includes(Number(PMAction))){
             alert("input check failed, please try again")
-            PMAction = prompt("Choose your action [1-atk, 2-Magic move,3-help]")
-        }*/
+            PMAction = Number(prompt("Choose your action [1-atk, 2-Magic move,3-help]"))
+        }
         if (PMAction == 1){
             console.log(`You dealt ${PATK - EDEF} damage to ${ENAME}`);
             EHP -= (PATK - EDEF);
         }
         else if(PMAction == '2'){
             let PSAction = prompt("Choose your magical move[1-Slice, 2-Shield, 3-Restore]")
-            /*while (PSAction != '1' || PSAction != '2' || PSAction != '3'){
+            while (!Choices.includes(Number(PSAction))) {
                 alert("input check failed, please try again")
-                let PSAction = prompt("Choose your magical move[1-Slice, 2-Shield, 3-Restore]")
-            }*/ 
+                PSAction = prompt("Choose your magical move[1-Slice, 2-Shield, 3-Restore]")
+            }
             if (PSAction == '1' && PMANA >= 15) {
                 console.log(`Slice halved enemy HP, reducing it to ${EHP / 2}`);
                 EHP = EHP / 2;
@@ -139,7 +150,7 @@ function battle(PNAME, PHP, PATK, PDEF, PMANA, ENAME, EHP, EATK, EDEF, EMANA){
                 alert("insufficient mana");
             }
         }
-        else{
+        else if(PMAction == "3"){
             help();
         }
         // resets
@@ -151,21 +162,21 @@ function battle(PNAME, PHP, PATK, PDEF, PMANA, ENAME, EHP, EATK, EDEF, EMANA){
         
 
         // enemy action
-            ENChoice = AI(EHP, EATK, EDEF, EMANA, EMAXHP);
-            if (ENChoice = 1) {
+            let ENChoice = AI(EHP, EATK, EDEF, EMANA, EMAXHP);
+            if (ENChoice == 1) {
                 console.log(`Enemy dealt ${EATK - PDEF} damage to ${PNAME}`);
                 PHP -= (EATK - PDEF);
             }
-            else if(ENChoice = 2){
+            else if(ENChoice == 2){
                 console.log(`Slice halved your HP, reducing it to ${PHP / 2}`);
                 PHP = PHP / 2;
                 EMANA -= 15;
-            }else if(ENChoice = 3){
+            }else if(ENChoice == 3){
                 console.log(`Shield doubles enemy def for 1 turn, setting it to ${EDEF * 2}`)
                 EDEF = EDEF * 2;
                 EMANA -= 10;
                 ETurn = 1;
-            }else if(ENChoice = 4){
+            }else if(ENChoice == 4){
                 console.log(`Restore heals for 50 hp, making enemy hp ${EHP + 50}`);
                 EHP += 50;
                 EMANA -= 7;
@@ -189,8 +200,9 @@ function battle(PNAME, PHP, PATK, PDEF, PMANA, ENAME, EHP, EATK, EDEF, EMANA){
     }
 }
 
-let winner = battle(PNAME, PHP, PATK, PDEF, PMANA, ENAME, EHP, EATK, EDEF, EMANA);
+let winner = battle(S1P.Name, S1P.HP, S1P.ATK, S1P.DEF, S1P.MANA, S1E.Name, S1E.HP, S1E.ATK, S1E.DEF, S1E.MANA);
 
+// Win check
 if (winner == "P"){
     alert("You win");
 }else if(winner == "E"){
