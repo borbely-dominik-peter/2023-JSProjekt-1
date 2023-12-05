99acb70fda35d214a8fe412a6049abf5860d7f2b
KissMartin <kiss.martin2@students.jedlik.eu>
2023-12-05 10:46:51 +0100

    navbar and background fixed

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// let images = document.querySelectorAll('.img');
-// for (i = 0; i < images.length; i++) {
-//    alert(`${images[i].clientWidth} x ${images[i].clientHeight}`);
-// }
+function expandOverlay(){
+    
+}
