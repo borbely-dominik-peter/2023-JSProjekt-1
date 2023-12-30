728ad04c219ce7944cfccaeb1063eeb170dd1ab7
KissMartin <kiss.martin2@students.jedlik.eu>
2023-12-30 12:30:40 +0100

    almost done

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-function expandOverlay(){
-    createOverlay();
-
-}
-
-function createOverlay(){
+function expandOverlay(clickedCard) {
     var overlay = document.createElement("div");
     overlay.id = "overlay";
     overlay.style.width = "100%";
@@ -11,39 +6,68 @@ function createOverlay(){
     overlay.style.position = "fixed";
     overlay.style.top = "0";
     overlay.style.left = "0";
-    overlay.style.right = "0";
-    overlay.style.bottom = "0";
-    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
-    overlay.style.zIndex = "2";
+    overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    overlay.style.zIndex = "10";
     document.body.appendChild(overlay);
-    var image = document.createElement("div");
-    image.id = "image";
-    image.style.width = "90%";
-    image.style.height = "90%";
-    image.style.position = "fixed";
-    image.style.top = "5%";
-    image.style.left = "5%";
-    image.style.right = "5%";
-    image.style.bottom = "5%";
-    image.style.backgroundColor = "white";
-    image.style.zIndex = "3";
-    document.body.appendChild(image);
-    var img = document.createElement("div");
-    img.src = "Images/placegolder.png";
-    img.style.width = "100%";
-    img.style.height = "100%";
-    img.style.backgroundColor = "#1f1f1f";
+
+    var cardRect = getCornersOfCard();
+
+    var contentContainer = document.createElement("div");
+    contentContainer.id = "content";
+    contentContainer.style.width = 1178 + "px";
+    contentContainer.style.height = 777 + "px";
+    contentContainer.style.position = "fixed";
+    contentContainer.style.top = cardRect[0] + "px";
+    contentContainer.style.left = cardRect[3] + "px";
+    contentContainer.style.bottom = cardRect[2] + "px";
+    contentContainer.style.right = cardRect[1] + "px";
+    contentContainer.style.zIndex = "11";
+    contentContainer.style.overflowY = "auto";
+    contentContainer.style.backgroundColor = "#1f1f1f";
+    
+    document.body.appendChild(contentContainer);
+
+    var duplicatedContent = document.createElement("div");
+    duplicatedContent.className = "duplicated-content";
+    duplicatedContent.style.position = "static";
+    duplicatedContent.style.cursor = "auto";
+
+    var clonedImage = clickedCard.querySelector(".img img").cloneNode(true);
+    var clonedText = clickedCard.querySelector(".container.programmerTitle").cloneNode(true);
+    console.log(clonedText);
+
+    clonedImage.className = "img";
+    clonedText.className = "container programmerTitle";
+
+    duplicatedContent.appendChild(clonedImage);
+    duplicatedContent.appendChild(clonedText);
+
+    contentContainer.appendChild(duplicatedContent);
+
     var close = document.createElement("button");
     close.id = "close";
     close.innerHTML = "Close";
     close.style.position = "absolute";
     close.style.top = "0";
     close.style.right = "0";
-    close.style.zIndex = "4";
-    close.onclick = function(){
+    close.style.zIndex = "13";
+    close.onclick = function () {
         document.body.removeChild(overlay);
-        document.body.removeChild(image);
-    }
-    image.appendChild(img);
-    image.appendChild(close);
+        document.body.removeChild(contentContainer);
+    };
+    contentContainer.appendChild(close);
+}
+
+
+function getCornersOfCard() {
+    var topRightCard = document.getElementById("top-right");
+    var bottomLeftCard = document.getElementById("bottom-left");
+    var topRightCardRect = topRightCard.getBoundingClientRect();
+    var bottomLeftCardRect = bottomLeftCard.getBoundingClientRect();
+    var corners = [];
+    corners.push(topRightCardRect.top);
+    corners.push(topRightCardRect.right);
+    corners.push(bottomLeftCardRect.bottom);
+    corners.push(bottomLeftCardRect.left);
+    return corners;
 }
